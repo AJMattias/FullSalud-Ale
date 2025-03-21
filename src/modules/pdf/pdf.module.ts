6e9ea612f2330136ab9ca4f7fba8407323433df0
AJMattias <mattias.alejandro@gmail.com>
2025-03-21 16:36:04 -0300

    cambios imports 3

--- src/modules/pdf/pdf.module.ts
+++ src/modules/pdf/pdf.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PdfService } from './pdf.service';
 import { PdfController } from './pdf.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MedicationRequest } from 'src/domain/entities/medication-request.entity';
+import { MedicationRequest } from '../../domain/entities/medication-request.entity';
+import { generateBarcode, generateQRCode } from '../../common/util/generateBarCode';
 
 @Module({
   imports:[TypeOrmModule.forFeature([MedicationRequest])],
   controllers: [PdfController],
-  providers: [PdfService, ],
+  providers: [PdfService, generateBarcode, generateQRCode ],
 })
 export class PdfModule {}
